9fdd3e7d4f86e2bab52f0ceb79d8fb2bb62154db
Omisha <omishaiyer13@gmail.com>
2026-08-07 14:18:34 +0530

    Enhance calculator with modulo and exponential options
    Added options for modulo and exponential operations.

--- calculator.cpp
+++ calculator.cpp
@@ -5,7 +5,7 @@ int main()
     cout<<"Enter 2 numbers: ";
     float a,b;
     cin>>a>>b;
-    cout<<"Enter 1 for +, 2 for -, 3 for *, 4 for /: 2";
+    cout<<"Enter 1 for +, 2 for -, 3 for *, 4 for /, 5 for modulo, 6 for exponential: ";
     int c;
     cin>>c;
 
@@ -19,14 +19,27 @@ int main()
         case 2:
         {
         cout<<"Difference: "<<a-b;
+        break;
         }
         case 3:
         {
         cout<<"Multiplication: "<<a*b;
+        break;
         }
         case 4:
         {
         cout<<"Division: "<<a/b;
+        break;
+        }
+        case 5:
+        {
+        cout<<"Modulo: "<<a%b;
+        break;
+        }
+        case 6:
+        {
+        cout<<"Exponential: "<<a^b;
+        break;
         }
         default:
         cout<<"Invalid choice";
