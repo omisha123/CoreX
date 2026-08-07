ee38c894a252beb1f06a80f6e9d6f465f965c2d7
Omisha <omishaiyer13@gmail.com>
2026-08-07 14:12:01 +0530

    Add calculator file with + and -

---
+++ calculator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Enter 2 numbers: ";
+    float a,b;
+    cin>>a>>b;
+    cout<<"Enter 1 for +, 2 for -, 3 for *, 4 for /: 2";
+    int c;
+    cin>>c;
+
+    switch(c)
+    {
+        case 1:
+        {
+        cout<<"Sum: "<<a+b;
+        break;
+        }
+        case 2:
+        {
+        cout<<"Difference: "<<a-b;
+        }
+        default:
+        cout<<"Invalid choice";
+    }
+    return 0;
+}
