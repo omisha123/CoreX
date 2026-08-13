8bcd148d6b313b7c8d4cbb835d68a6e0ce31e474
akshitamahapatra703-gif <akshitamahapatra703@gmail.com>
2026-08-13 22:08:52 +0530

    modified the code

--- calculator.cpp
+++ calculator.cpp
@@ -7,31 +7,32 @@ int main(){
     cout << "Enter 2 numbers: ";
     cin >> a >> b;
 
-    cout << "1.Add(+)\n2. Subtract(-)\n3. Multiply(*)\n4. Divide(/)\n5.Modulus(%)\n6. Power(^)\n";
+    cout << "1. Add(+)\n2. Subtract(-)\n3. Multiply(*)\n4. Divide(/)\n5. Modulus(%)\n6. Power(^)\n";
     cout << "Enter your choice: ";
     cin >> c;
 
     switch(c){
         case 1:
-            cout << "Result: " << a + b;
+            cout << "Result: " << a + b<<endl;
             break;
         case 2:
-            cout << "Result: " << a - b;
+            cout << "Result: " << a - b<<endl;
             break;
         case 3:
-            cout << "Result: " << a * b;
+            cout << "Result: " << a * b<<endl;
             break;
         case 4:
-            cout << "Result: " << a / b;
+            cout << "Result: " << a / b<<endl;
             break;
         case 5:
-            cout << "Result: " << a % b;
+            cout << "Result: " << a % b<<endl;
             break;
         case 6:
-            cout << "Result: " << pow(a, b);
+            cout << "Result: " << pow(a, b)<<endl;
             break;
         default:
-            cout << "Invalid choice";
+            cout << "Invalid choice"<<endl;
+            break;
     }
     return 0;
 }
